a386a5d39a6bb8cf425cfb3573e9170f71b40ebb
Connor Knabe <connorknabe@gmail.com>
2014-09-30 16:01:25 -0500

    Using request on

--- codeschool/readData.js
+++ codeschool/readData.js
@@ -2,12 +2,12 @@ var http = require('http');
 
 http.createServer(function(request, response){
 	response.writeHead(200);
-	console.log("Dog is running.");
 	request.on('data',function(chunk){
+		response.write(chunk);
 		console.log(chunk.toString());
 	});
 	request.on('end', function(){
 		response.end();
 	});
 
-});
+}).listen(1337)
