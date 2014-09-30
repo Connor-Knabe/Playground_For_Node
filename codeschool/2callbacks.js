fd602f9116c7dfec9cdef947eadf17e7a92915f3
Connor Knabe <connorknabe@gmail.com>
2014-09-30 15:46:27 -0500

    Easier to visualize using console.log

--- codeschool/2callbacks.js
+++ codeschool/2callbacks.js
@@ -2,9 +2,8 @@ var http = require('http');
 
 http.createServer(function(request, response){
 	response.writeHead(200);
-	response.write("Dog is running.");
+	console.log("Dog is running.");
 	setTimeout(function(){
-		response.write("Dog is done.");
-		response.end();
+		console.log("Dog is done.");
 	}, 5000);
 }).listen(1337);
