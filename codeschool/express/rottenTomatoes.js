5c7d672cd8904f04e0db56782560a9cc31b4972d
Connor Knabe <connorknabe@gmail.com>
2014-10-01 10:28:25 -0500

    Works properlY

--- codeschool/express/rottenTomatoes.js
+++ codeschool/express/rottenTomatoes.js
@@ -7,13 +7,15 @@ var request = require('request');
 
 
 console.log("HI");
-app.get('/apikey/:api', function(req, response){
+app.get('/apikey/:api/q/:query', function(req, response){
 	var api = req.params.api;
+	var query = req.params.query;
+
 	options = {
 		protocol: "http:",
 		host: 'api.rottentomatoes.com',
 		pathname: '/api/public/v1.0/movies.json',
-		query: { api_key: api}
+		query: { apikey: api, q:query}
 	}
 	var rottenUrl = url.format(options);
 	console.log(rottenUrl);
