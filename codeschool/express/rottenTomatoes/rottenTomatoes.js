2aebc8bc2b5a71e68959b9ef2b8ebdcf55944efa
Connor Knabe <connorknabe@gmail.com>
2014-10-02 14:31:39 -0500

    Moved to a new folder

---
+++ codeschool/express/rottenTomatoes/rottenTomatoes.js
@@ -0,0 +1,30 @@
+// Usage:
+//url:port/apikey/(insert key)/q/(insert movie)/page_limit/(insert page limit)
+
+var express = require('express');
+var http = require('http');
+var app = express.createServer();
+var url = require('url');
+var request = require('request');
+
+app.get('/apikey/:api/q/:query/page_limit/:limit', function(req, response){
+	var api = req.params.api;
+	var query = req.params.query;
+	var limit = req.params.limit;
+
+	options = {
+		protocol: "http:",
+		host: 'api.rottentomatoes.com',
+		pathname: '/api/public/v1.0/movies.json',
+		query: { apikey: api, q:query, page_limit:limit}
+	}
+	var rottenUrl = url.format(options);
+	console.log(rottenUrl);
+	//request(rottenUrl).pipe(response);
+	request(rottenUrl, function(err, res, body){
+		var results = JSON.parse(body);
+		response.render('movies.ejs'), {id: id, title: title});
+
+	})
+});
+app.listen(1337);
