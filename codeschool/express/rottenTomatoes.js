b17e69d2b2da83f01f6bcc9722a7570f4d5da5cf
Connor Knabe <connorknabe@gmail.com>
2014-10-02 14:23:05 -0500

    Added comment on how to use

--- codeschool/express/rottenTomatoes.js
+++ codeschool/express/rottenTomatoes.js
@@ -1,8 +1,12 @@
+// Usage:
+//url:port/apikey/(insert key)/q/(insert movie)/page_limit/(insert page limit)
+
 var express = require('express');
 var http = require('http');
 var app = express.createServer();
 var url = require('url');
 var request = require('request');
+
 app.get('/apikey/:api/q/:query/page_limit/:limit', function(req, response){
 	var api = req.params.api;
 	var query = req.params.query;
@@ -12,7 +16,7 @@ app.get('/apikey/:api/q/:query/page_limit/:limit', function(req, response){
 		protocol: "http:",
 		host: 'api.rottentomatoes.com',
 		pathname: '/api/public/v1.0/movies.json',
-		query: { apikey: api, q:query page_limit:limit}
+		query: { apikey: api, q:query, page_limit:limit}
 	}
 	var rottenUrl = url.format(options);
 	console.log(rottenUrl);
