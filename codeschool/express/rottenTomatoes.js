8f8b735b6c95dfe6062d7e4e04da4a0980f4eb8c
Connor Knabe <connorknabe@gmail.com>
2014-10-02 14:13:12 -0500

    Removed the log statements

--- codeschool/express/rottenTomatoes.js
+++ codeschool/express/rottenTomatoes.js
@@ -3,14 +3,9 @@ var http = require('http');
 var app = express.createServer();
 var url = require('url');
 var request = require('request');
-
-
-
-console.log("HI");
 app.get('/apikey/:api/q/:query', function(req, response){
 	var api = req.params.api;
 	var query = req.params.query;
-
 	options = {
 		protocol: "http:",
 		host: 'api.rottentomatoes.com',
@@ -21,6 +16,4 @@ app.get('/apikey/:api/q/:query', function(req, response){
 	console.log(rottenUrl);
 	request(rottenUrl).pipe(response);
 });
-console.log("Ha");
-
 app.listen(1337);
