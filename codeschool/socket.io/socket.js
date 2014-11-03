8d83076336be3e839bf406bfda3898873df016c0
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-03 08:49:06 -0600

    Added socket.io listener

--- codeschool/socket.io/socket.js
+++ codeschool/socket.io/socket.js
@@ -1,1 +1,14 @@
 var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connect',function(client){
+	console.log('Client connected...');
+
+
+});
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+server.llisten(1337);
