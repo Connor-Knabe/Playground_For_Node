9e43c8eebedac8aaa5cac778c9479fb644d7ca30
Connor Knabe <ConnorKnabe@gmail.com>
2014-11-03 08:44:58 -0600

    Made changes to rottenTomatoes file

--- codeschool/express/rottenTomatoes.js
+++ codeschool/express/rottenTomatoes.js
@@ -1,12 +1,10 @@
 // Usage:
 //url:port/apikey/(insert key)/q/(insert movie)/page_limit/(insert page limit)
-
 var express = require('express');
 var http = require('http');
 var app = express.createServer();
 var url = require('url');
 var request = require('request');
-
 app.get('/apikey/:api/q/:query/page_limit/:limit', function(req, response){
 	var api = req.params.api;
 	var query = req.params.query;
@@ -21,6 +19,5 @@ app.get('/apikey/:api/q/:query/page_limit/:limit', function(req, response){
 	var rottenUrl = url.format(options);
 	console.log(rottenUrl);
 	request(rottenUrl).pipe(response);
-
 });
 app.listen(1337);
--- codeschool/fileStuff/files.js
+++ codeschool/fileStuff/files.js
@@ -3,4 +3,6 @@ var fs = require('fs');
 var file = fs.createReadStream("input.txt");
 var newFile = fs.createWriteStream("output.txt");
 
+
+
 file.pipe(newFile);
